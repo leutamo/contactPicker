6e2a394e662374b4c750fb8cb0ded8dc7784e909
Gian <giancarlos.araya@erictel.com>
2020-04-02 01:42:37 -0500

    First Commit

--- ContactPicker/ViewController.m
+++ ContactPicker/ViewController.m
@@ -6,6 +6,9 @@
 //  Copyright © 2020 erictel-imac. All rights reserved.
 //
 
+@import UIKit;
+@import ContactsUI;
+
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -19,5 +22,31 @@
     // Do any additional setup after loading the view.
 }
 
+
+- (IBAction)btnBuscar:(id)sender {
+    
+    CNContactPickerViewController *contactPicker = [[CNContactPickerViewController alloc]init];
+    
+    contactPicker.delegate = self;
+    contactPicker.displayedPropertyKeys = @[CNContactPhoneNumbersKey];
+    
+    [self presentViewController:contactPicker animated:YES completion:nil];
+}
+
+-(void) contactPicker:(CNContactPickerViewController *) picker didSelectContactProperty:(nonnull CNContactProperty *)contactProperty{
+    
+    CNContact *contact = contactProperty.contact;
+    NSString *identify = contactProperty.identifier;
+    NSString *_lastDisplay = @"";
+    for (CNLabeledValue<CNPhoneNumber*>* number in contact.phoneNumbers) {
+        
+        if ([number.identifier isEqualToString:identify]) {
+            _lastDisplay = ((CNPhoneNumber *)number.value).stringValue;
+            self.txtNumero.text = _lastDisplay;
+        }
+        
+        
+    }
+}
 
 @end
